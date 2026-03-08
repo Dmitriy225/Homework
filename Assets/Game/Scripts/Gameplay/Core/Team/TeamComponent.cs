dca9dbae4843209ba1f6bc7bc6fa2fdf1b54e08d
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-08 13:14:23 +0600

    Создан TeamComponent

---
+++ Assets/Game/Scripts/Gameplay/Core/Team/TeamComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public sealed class TeamComponent : MonoBehaviour
+    {
+        public event Action<TeamType> OnTeamChanged;
+
+        [SerializeField]
+        private TeamType _team;
+
+        public void SetTeam(TeamType team)
+        {
+            if (_team == team)
+            {
+                return;
+            }
+
+            _team = team;
+            OnTeamChanged?.Invoke(team);
+        }
+    }
+}
