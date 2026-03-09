2e7a7343531385699603bac2e2743b9300606118
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-09 18:12:20 +0600

    Создан FireVfxComponent.cs

---
+++ Assets/Game/Scripts/Gameplay/Core/Combat/FireVfxComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public sealed class FireVfxComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private FireComponent _fireComponent;
+
+        [SerializeField]
+        private ParticleSystem _fireVFX;
+
+        private void OnEnable()
+        {
+            _fireComponent.OnFire += _fireVFX.Play;
+        }
+
+        private void OnDisable()
+        {
+            _fireComponent.OnFire -= _fireVFX.Play;
+        }
+    }
+}
--- Assets/Game/Scripts/Gameplay/Entities/Player/PlayerShipView.cs
+++ Assets/Game/Scripts/Gameplay/Entities/Player/PlayerShipView.cs
@@ -12,12 +12,6 @@ namespace Game
         private FireComponent _fireComponent;
 
         [SerializeField]
-        private Renderer _renderer;
-
-        [SerializeField]
-        private Material _material;
-
-        [SerializeField]
         private DamageViewComponent _damageViewComponent;
 
         [SerializeField]
@@ -30,16 +24,12 @@ namespace Game
         private AudioClip _damageSFX;
 
         [SerializeField]
-        private ParticleSystem _fireVFX;
-
-        [SerializeField]
         private CameraShaker _cameraShaker;
 
         private void Awake()
         {
             _healthComponent = _ship.GetComponent<HealthComponent>();
             _fireComponent = _ship.GetComponent<FireComponent>();
-            _renderer.material = _material;
         }
 
         private void OnEnable()
@@ -57,7 +47,6 @@ namespace Game
         private void OnFire()
         {
             _audioSource.PlayOneShot(_fireSFX);
-            _fireVFX.Play();
         }
 
         private void OnHealthReduced()
