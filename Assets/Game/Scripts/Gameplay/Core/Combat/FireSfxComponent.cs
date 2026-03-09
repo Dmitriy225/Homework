2da32f630d442563eaa5e662cad4b70ab01eefe6
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-09 18:15:11 +0600

    Create FireSfxComponent.cs

---
+++ Assets/Game/Scripts/Gameplay/Core/Combat/FireSfxComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public sealed class FireSfxComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private FireComponent _fireComponent;
+
+        [SerializeField]
+        private AudioSource _audioSource;
+
+        [SerializeField]
+        private AudioClip _fireSFX;
+
+        private void OnEnable()
+        {
+            _fireComponent.OnFire += OnFire;
+        }
+
+        private void OnDisable()
+        {
+            _fireComponent.OnFire -= OnFire;
+        }
+
+        private void OnFire()
+        {
+            _audioSource.PlayOneShot(_fireSFX);
+        }
+    }
+}
