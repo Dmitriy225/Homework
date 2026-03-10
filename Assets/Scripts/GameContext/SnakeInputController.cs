c143881293b1eb49ef746dc0a5b1acb4e8c33a85
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-10 23:46:54 +0600

    Создан SnakeInputController.cs

---
+++ Assets/Scripts/GameContext/SnakeInputController.cs
@@ -0,0 +1,38 @@
+﻿using Modules;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+    public sealed class SnakeInputController : ITickable
+    {
+        private const string HORIZONTAL = "Horizontal";
+        private const string VERTICAL = "Vertical";
+        private readonly ISnake _snake;
+
+        public SnakeInputController(ISnake snake)
+        {
+            _snake = snake;
+        }
+
+        public void Tick()
+        {
+            if (Input.GetAxisRaw(HORIZONTAL) == -1)
+            {
+                _snake.Turn(SnakeDirection.LEFT);
+            }
+            else if (Input.GetAxisRaw(HORIZONTAL) == 1)
+            {
+                _snake.Turn(SnakeDirection.RIGHT);
+            }
+            else if (Input.GetAxisRaw(VERTICAL) == 1)
+            {
+                _snake.Turn(SnakeDirection.UP);
+            }
+            else if (Input.GetAxisRaw(VERTICAL) == -1)
+            {
+                _snake.Turn(SnakeDirection.DOWN);
+            }
+        }
+    }
+}
