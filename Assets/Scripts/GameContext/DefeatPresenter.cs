599108929bf2de284964092addb00f9437d81762
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-11 00:15:10 +0600

    Создан DefeatPresenter.cs

---
+++ Assets/Scripts/GameContext/DefeatPresenter.cs
@@ -0,0 +1,47 @@
+﻿using Modules;
+using SnakeGame;
+using System;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+    public sealed class DefeatPresenter : IInitializable, IDisposable
+    {
+        private readonly ISnake _snake;
+        private readonly IWorldBounds _worldBounds;
+        private readonly IGameUI _gameUI;
+
+        public DefeatPresenter(ISnake snake, IWorldBounds worldBounds, IGameUI gameUI)
+        {
+            _snake = snake;
+            _worldBounds = worldBounds;
+            _gameUI = gameUI;
+        }
+
+        public void Initialize()
+        {
+            _snake.OnMoved += OnSnakeMoved;
+            _snake.OnSelfCollided += OnSnakeSelfCollided;
+        }
+
+        public void Dispose()
+        {
+            _snake.OnMoved -= OnSnakeMoved;
+            _snake.OnSelfCollided -= OnSnakeSelfCollided;
+        }
+
+        private void OnSnakeMoved(Vector2Int position)
+        {
+            if (!_worldBounds.IsInBounds(position))
+            {
+                _gameUI.GameOver(false);
+            }
+        }
+
+        private void OnSnakeSelfCollided()
+        {
+            _gameUI.GameOver(false);
+        }
+    }
+}
