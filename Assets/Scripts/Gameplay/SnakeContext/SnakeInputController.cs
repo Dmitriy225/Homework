e30914386a5e4e9f18e00189a24e2b799aeb55d4
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-11 23:48:16 +0600

    Update SnakeInputController.cs

--- Assets/Scripts/Gameplay/SnakeContext/SnakeInputController.cs
+++ Assets/Scripts/Gameplay/SnakeContext/SnakeInputController.cs
@@ -6,8 +6,8 @@ namespace SnakeGame
 {
     public sealed class SnakeInputController : ITickable
     {
-        private const string HORIZONTAL = "Horizontal";
-        private const string VERTICAL = "Vertical";
+        private const string Horizontal = "Horizontal";
+        private const string Vertical = "Vertical";
         private readonly ISnake _snake;
 
         public SnakeInputController(ISnake snake)
@@ -17,19 +17,19 @@ namespace SnakeGame
 
         public void Tick()
         {
-            if (Input.GetAxisRaw(HORIZONTAL) == -1)
+            if (Input.GetAxisRaw(Horizontal) == -1)
             {
                 _snake.Turn(SnakeDirection.LEFT);
             }
-            else if (Input.GetAxisRaw(HORIZONTAL) == 1)
+            else if (Input.GetAxisRaw(Horizontal) == 1)
             {
                 _snake.Turn(SnakeDirection.RIGHT);
             }
-            else if (Input.GetAxisRaw(VERTICAL) == 1)
+            else if (Input.GetAxisRaw(Vertical) == 1)
             {
                 _snake.Turn(SnakeDirection.UP);
             }
-            else if (Input.GetAxisRaw(VERTICAL) == -1)
+            else if (Input.GetAxisRaw(Vertical) == -1)
             {
                 _snake.Turn(SnakeDirection.DOWN);
             }
