c70aac4d7ed8cc786edb9de8bb50ff3291dbd7be
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-11 23:06:04 +0600

    Зарегистрирован CoinPool

--- Assets/Scripts/Gameplay/GameContext/GameContextInstaller.cs
+++ Assets/Scripts/Gameplay/GameContext/GameContextInstaller.cs
@@ -1,7 +1,6 @@
 using Modules;
 using UnityEngine;
 using Zenject;
-using Zenject.SpaceFighter;
 
 namespace SnakeGame
 {
@@ -10,12 +9,25 @@ namespace SnakeGame
         [SerializeField]
         private WorldBounds _worldBounds;
 
+        [SerializeField]
+        private Coin _coinPrefab;
+
+        [SerializeField]
+        private Transform _coinContainer;
+
         public override void InstallBindings()
         {
             Container
                 .Bind<IWorldBounds>()
                 .To<WorldBounds>()
                 .FromInstance(_worldBounds)
+                .AsSingle();
+
+            Container
+                .BindMemoryPoolCustomInterface<ICoin, CoinPool, IMemoryPool<Vector2Int, Coin>>()
+                .WithInitialSize(9)
+                .FromComponentInNewPrefab(_coinPrefab)
+                .UnderTransform(_coinContainer)
                 .AsSingle();
         }
     }
