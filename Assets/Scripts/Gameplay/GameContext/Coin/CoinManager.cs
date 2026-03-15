ec5fe83eada958a90182c9b78d920536f75f9717
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-15 23:44:27 +0600

    Update CoinManager.cs

--- Assets/Scripts/Gameplay/GameContext/Coin/CoinManager.cs
+++ Assets/Scripts/Gameplay/GameContext/Coin/CoinManager.cs
@@ -13,6 +13,7 @@ namespace SnakeGame
         private readonly IMemoryPool<Vector2Int, Coin> _coinPool;
         private readonly IWorldBounds _worldBounds;
         private readonly Dictionary<Vector2Int, ICoin> _coinMap = new();
+        private readonly HashSet<ICoin> _coins = new();
 
         public CoinManager(
             IMemoryPool<Vector2Int, Coin> coinPool,
@@ -23,14 +24,15 @@ namespace SnakeGame
             _worldBounds = worldBounds;
         }
 
-        public bool Despawn(Vector2Int key)
+        public bool Despawn(ICoin coin)
         {
-            if (!_coinMap.TryGetValue(key, out var coin))
+            if (!_coins.Contains(coin))
             {
                 return false;
             }
 
-            _coinMap.Remove(key);
+            _coinMap.Remove(coin.Position);
+            _coins.Remove(coin);
             _coinPool.Despawn(coin);
 
             if (_coinMap.Keys.Count == 0)
@@ -46,7 +48,9 @@ namespace SnakeGame
             for (int i = 0; i < count; i++)
             {
                 var key = _worldBounds.GetRandomPosition();
-                _coinMap.Add(key, _coinPool.Spawn(key));
+                var coin = _coinPool.Spawn(key);
+                _coinMap.Add(key, coin);
+                _coins.Add(coin);
             }
         }
 
