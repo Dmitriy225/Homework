9708eeb31e533b78eb4b6bf35fa1ab352308e2da
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-15 22:53:03 +0600

    Добавлен LevelSwitchObserver.cs

---
+++ Assets/Scripts/Gameplay/GameContext/Level/LevelSwitchObserver.cs
@@ -0,0 +1,34 @@
+﻿using Modules;
+using System;
+using Zenject;
+
+namespace SnakeGame
+{
+    public sealed class LevelSwitchObserver : IInitializable, IDisposable
+    {
+        private readonly IDifficulty _difficulty;
+        private readonly CoinManager _coinManager;
+
+        public LevelSwitchObserver(IDifficulty difficulty, CoinManager coinManager)
+        {
+            _difficulty = difficulty;
+            _coinManager = coinManager;
+        }
+
+        public void Initialize()
+        {
+            SpawnCoins();
+            _difficulty.OnStateChanged += SpawnCoins;
+        }
+
+        public void Dispose()
+        {
+            _difficulty.OnStateChanged -= SpawnCoins;
+        }
+
+        private void SpawnCoins()
+        {
+            _coinManager.Spawn(_difficulty.Current + 1);
+        }
+    }
+}
