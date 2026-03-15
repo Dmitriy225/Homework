a00365af1ca10e9d1c42982fc72439086eafdbab
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-15 23:44:56 +0600

    Добавлен CoinDespawnController.cs

---
+++ Assets/Scripts/Gameplay/GameContext/Coin/CoinDespawnController.cs
@@ -0,0 +1,36 @@
+﻿using Modules;
+using System;
+using UnityEngine;
+using Zenject;
+
+namespace SnakeGame
+{
+    public sealed class CoinDespawnController : IInitializable, IDisposable
+    {
+        private readonly SnakeCoinConsumer _coinConsumer;
+        private readonly CoinManager _coinManager;
+
+        public CoinDespawnController(
+            SnakeCoinConsumer coinConsumer,
+            CoinManager coinManager)
+        {
+            _coinConsumer = coinConsumer;
+            _coinManager = coinManager;
+        }
+
+        public void Initialize()
+        {
+            _coinConsumer.OnConsumed += Despawn;
+        }
+
+        public void Dispose()
+        {
+            _coinConsumer.OnConsumed -= Despawn;
+        }
+
+        private void Despawn(ICoin coin)
+        {
+            _coinManager.Despawn(coin);
+        }
+    }
+}
