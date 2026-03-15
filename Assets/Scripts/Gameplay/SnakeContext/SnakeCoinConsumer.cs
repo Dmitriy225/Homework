7ffa74b749a3f8265b428111c393ba7c5dd57436
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-15 22:46:31 +0600

    Добавлен SnakeCoinConsumer.cs

---
+++ Assets/Scripts/Gameplay/SnakeContext/SnakeCoinConsumer.cs
@@ -0,0 +1,23 @@
+﻿using Modules;
+using System;
+
+namespace SnakeGame
+{
+    public sealed class SnakeCoinConsumer
+    {
+        public event Action<ICoin> OnConsumed;
+
+        private readonly ISnake _snake;
+
+        public SnakeCoinConsumer(ISnake snake)
+        {
+            _snake = snake;
+        }
+
+        public void Consume(ICoin coin)
+        {
+            _snake.Expand(coin.Bones);
+            OnConsumed?.Invoke(coin);
+        }
+    }
+}
