d2c5c3449ada05e89f15aa762a21094588638c4d
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-17 18:40:32 +0600

    Создан PlanetPresenter

---
+++ Assets/Game/Scripts/Presenters/PlanetPresenter.cs
@@ -0,0 +1,36 @@
+﻿using Game.Views;
+using Modules.Planets;
+using System;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Presenters
+{
+    public sealed class PlanetPresenter : IInitializable, IDisposable
+    {
+        private readonly PlanetView _view;
+        private readonly IPlanet _planet;
+
+        public PlanetPresenter(PlanetView view, IPlanet planet)
+        {
+            _view = view;
+            _planet = planet;
+        }
+
+        public void Initialize()
+        {
+            UpdateLockState();
+            _planet.OnUnlocked += UpdateLockState;
+        }
+
+        public void Dispose()
+        {
+            _planet.OnUnlocked -= UpdateLockState;
+        }
+
+        private void UpdateLockState()
+        {
+            _view.SetImageIcon(_planet.GetIcon(_planet.IsUnlocked));
+        }
+    }
+}
