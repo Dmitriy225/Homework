5b5e7770d2c09607e5d825602fa6bc55491bec22
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-17 18:40:16 +0600

    Создан PlanetView

---
+++ Assets/Game/Scripts/Views/PlanetView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Views
+{
+    public sealed class PlanetView : MonoBehaviour
+    {
+        [SerializeField]
+        private Image _image;
+
+        public void SetImageIcon(Sprite icon)
+        {
+            _image.sprite = icon;
+        }
+    }
+}
