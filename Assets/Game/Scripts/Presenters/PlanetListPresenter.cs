47f9b6ce19321d0b939e22902752774059fdbe72
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-03-19 22:54:06 +0600

    Создан PlanetListPresenter.cs

---
+++ Assets/Game/Scripts/Presenters/PlanetListPresenter.cs
@@ -0,0 +1,39 @@
+﻿using Game.Views;
+using Modules.Planets;
+using System;
+using System.Collections.Generic;
+using Zenject;
+
+namespace Game.Presenters
+{
+    public sealed class PlanetListPresenter : IInitializable, IDisposable
+    {
+        private readonly PlanetView[] _views;
+        private readonly IPlanet[] _planets;
+        private readonly List<PlanetPresenter> _presenters = new();
+
+        public PlanetListPresenter(PlanetView[] views, IPlanet[] planets)
+        {
+            _views = views;
+            _planets = planets;
+        }
+
+        public void Initialize()
+        {
+            for (int i = 0; i < _planets.Length; i++)
+            {
+                var presenter = new PlanetPresenter(_views[i], _planets[i]);
+                _presenters.Add(presenter);
+                presenter.Initialize();
+            }
+        }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < _presenters.Count; i++)
+            {
+                _presenters[i].Dispose();
+            }
+        }
+    }
+}
