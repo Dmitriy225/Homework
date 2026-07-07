302269ffe7a40afdc27982d9f46d9e7f5d68601b
Dmitriy225 <161407378+Dmitriy225@users.noreply.github.com>
2026-07-07 19:03:28 +0600

    Добавлена папка Movement

---
+++ Assets/Game/Scripts/Gameplay/Entities/Core/Movement/MoveProcedures.cs
@@ -0,0 +1,15 @@
+﻿using Atomic.Entities;
+using UnityEngine;
+
+namespace Game
+{
+    public static class MoveProcedures
+    {
+        public static void MoveStep(this IEntity entity, Vector3 direction, float deltaTime)
+        {
+            var position = entity.GetPosition();
+            var speed = entity.GetMoveSpeed();
+            position.Value += speed.Value * deltaTime * direction.normalized;
+        }
+    }
+}
